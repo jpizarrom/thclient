ba6206b7e0094fbf129c82fe8919356b2f6cada0
Juan Pizarro <jpizarro@doingit.cl>
2010-07-30 22:41:38 -0400

    add THHelper interface

---
+++ src/com/jpizarro/th/client/model/util/http/THHelper.java
@@ -0,0 +1,51 @@
+package com.jpizarro.th.client.model.util.http;
+
+import java.util.List;
+
+import com.jpizarro.th.client.model.service.to.GameCTO;
+import com.jpizarro.th.client.model.service.to.response.GenericGameResponseTO;
+import com.jpizarro.th.entity.GameTO;
+import com.jpizarro.th.entity.TeamTO;
+import com.jpizarro.th.entity.UserTO;
+
+public interface THHelper {
+	public UserTO login(String userName, String password) 
+	throws Exception;
+	
+	public void logout(String login) 
+	throws Exception;
+	
+	public boolean registerUser() 
+	throws Exception;
+	
+	public boolean updateUser() 
+	throws Exception;
+	
+	public boolean changePassword(String oldPassword, String newPassword) 
+	throws Exception;
+	
+	public List<String> findCitiesWithGames() throws Exception;
+	
+	public GameTO findGame(long gameId)  throws Exception;
+	
+	public boolean joinGame(long gameId, long teamId) 
+	throws Exception;
+	
+	public GenericGameResponseTO updateLocation(int latitude, int longitude) 
+	throws Exception;
+	
+	public boolean sendMessage(String receiverLogin, String body) 
+	throws Exception;
+	
+	public GameCTO findGamesByCity(String city, int startIndex, int count) 
+	throws Exception;
+	
+	public GenericGameResponseTO startOrContinueGame(String login) 
+	throws Exception;
+	
+	public List<TeamTO> findTeamsByGame(long gameId, int startIndex, int count) 
+	throws Exception;
+	
+	public GenericGameResponseTO takePlace(long id, int latitude, int longitude) 
+	throws Exception;
+}
